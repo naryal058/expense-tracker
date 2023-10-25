1b7444d4e21ed2701e9ae799d1d56481095f103a
Nikhil Aryal <aryaln0058@gmail.com>
2023-10-25 14:43:18 -0500

    updated expenses so far and remaining budget

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ const ExpenseItem = (props) => {
         <li className="list-group-item d-flex justify-content-between align-items-center">
             {props.name}
         <div>
-            <span className="badge badge-primary badge-pill mr-3">
+            <span className="mr-3">
                 ${props.cost}
             </span>
             <TiDelete size = '1.5rem'></TiDelete>
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const ExpenseTotal = () => {
+    const { expenses } = useContext(AppContext);
+    
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     return (
         <div className='alert alert-primary'>
-			<span>Spent so far: £1000</span>
+			<span>Spent so far: ${totalExpenses}</span>
 		</div>
     )
 }
