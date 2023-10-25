0fd9d60bc466126554fdde9b9567667cf16b01f8
Nikhil Aryal <aryaln0058@gmail.com>
2023-10-25 14:51:54 -0500

    made the delete icon working

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TiDelete } from 'react-icons/ti';
+import { AppContext } from "../context/AppContext";
 
 const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id
+        })
+    }
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
             {props.name}
@@ -9,7 +18,7 @@ const ExpenseItem = (props) => {
             <span className="mr-3">
                 ${props.cost}
             </span>
-            <TiDelete size = '1.5rem'></TiDelete>
+            <TiDelete size = '1.5rem' onClick={handleDeleteExpense}></TiDelete>
         </div>
         </li>
     )
